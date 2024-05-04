f59986079c20892c03baf2e20137680cdf1b9b6d
loshan <it21803970@my.sliit.lk>
2024-05-04 22:08:13 +0530

    some changes for deploy

--- server/server.js
+++ server/server.js
@@ -6,13 +6,14 @@ const app = express()
 const cors = require('cors')
 const auth = require('./routes/auth')
 const path = require('path');
+//const cc = require('../nasa_app/dist/index.html')
 dotenv.config()
 mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser : true})
 const con = mongoose.connection 
 con.on('open',()=>{
     console.log('mongoDB connected.....')
 })
-const _dirname = path.resolve()
+const __dirname = path.resolve()
 //middleware
 app.use(express.json())
 app.use(cors(
@@ -26,7 +27,7 @@ app.use(cookieParser());
 //user route middleware 
  app.use('/api/user',auth)
 
- 
+
 // app.use(express.static(path.join(_dirname,'/nasa_app/dist')))
 // app.get('*',(req,res)=>{
 //     res.sendFile(path.join(_dirname,'nasa_app','dist','index.html'))
@@ -42,7 +43,7 @@ app.use(express.static(path.join(__dirname, '../nasa_app/dist')));
 
 // Serve the index.html file for all other routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../nasa_app/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../nasa_app/dist/index.html'));
 });
 
 app.listen(8080,()=>{
