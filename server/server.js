4305b465c14499900a2f2a70ca5cb0dedd579956
loshan <it21803970@my.sliit.lk>
2024-05-04 17:06:49 +0530

    for deploy

--- server/server.js
+++ server/server.js
@@ -5,13 +5,14 @@ const cookieParser = require('cookie-parser'); // Import cookie-parser
 const app = express()
 const cors = require('cors')
 const auth = require('./routes/auth')
+import path from 'path'
 dotenv.config()
 mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser : true})
 const con = mongoose.connection 
 con.on('open',()=>{
     console.log('mongoDB connected.....')
 })
-
+const _dirname = path.resolve()
 //middleware
 app.use(express.json())
 app.use(cors(
@@ -24,6 +25,10 @@ app.use(cors(
 app.use(cookieParser());
 //user route middleware 
 app.use('/api/user',auth)
+app.use(express.static(path.join(_dirname,'/nasa_app/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(_dirname,'nasa_app','dist','index.html'))
+})
 app.listen(8080,()=>{
     console.log("server running on port 8080.....")
 })
