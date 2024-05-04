15df326f283f85910b1ec66700621f84f4c4a64d
loshan <it21803970@my.sliit.lk>
2024-05-04 22:51:00 +0530

    some changes for deploy

--- server/server.js
+++ server/server.js
@@ -1,36 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser'); // Import cookie-parser
-const app = express()
-const cors = require('cors')
-const auth = require('./routes/auth')
+const app = express();
+const cors = require('cors');
+const auth = require('./routes/auth');
 const path = require('path');
-//const cc = require('../nasa_app/dist/index.html')
-dotenv.config()
-mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser : true})
-const con = mongoose.connection 
-con.on('open',()=>{
-    console.log('mongoDB connected.....')
-})
-const _dirname = path.resolve()
-//middleware
-app.use(express.json())
-app.use(cors())
+
+dotenv.config();
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true });
+const con = mongoose.connection;
+con.on('open', () => {
+    console.log('mongoDB connected.....');
+});
+
+const _dirname = path.resolve();
+
+// Middleware
+app.use(express.json());
+app.use(cors({
+    origin: 'https://nasa-api-w7ng.onrender.com' // Update with your frontend domain over HTTPS
+}));
 app.use(cookieParser());
-//user route middleware 
- app.use('/api/user',auth)
 
-
-// app.use(express.static(path.join(_dirname,'/nasa_app/dist')))
-// app.get('*',(req,res)=>{
-//     res.sendFile(path.join(_dirname,'nasa_app','dist','index.html'))
-// })
-// app.use(express.static(path.join(_dirname, '/nasa_app')))
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(_dirname, 'nasa_app', 'index.html'))
-// })
+// User route middleware 
+app.use('/api/user', auth);
 
 // Serve static files from the frontend build directory
 app.use(express.static(path.join(__dirname, '../nasa_app/dist')));
@@ -40,8 +34,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../nasa_app/dist/index.html'));
 });
 
-app.listen(8080,()=>{
-    console.log("server running on port 8080.....")
-})
-
-
+app.listen(8080, () => {
+    console.log("Server running on port 8080.....");
+});
